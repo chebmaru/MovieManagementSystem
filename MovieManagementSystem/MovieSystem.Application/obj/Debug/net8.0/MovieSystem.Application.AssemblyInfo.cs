fba8c9c1adad2434bd021f47cf820b31afcc6898
chebm <chebm@MARU>
2024-11-13 22:16:47 +0100

    add Domain layer to application layer dependcies

--- MovieManagementSystem/MovieSystem.Application/obj/Debug/net8.0/MovieSystem.Application.AssemblyInfo.cs
+++ MovieManagementSystem/MovieSystem.Application/obj/Debug/net8.0/MovieSystem.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27c6588e34de6a78d52744876ac9714f46ac22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4642bfb4a3228770579775c0643d0a075aa718e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
