2fcf811849d0d660f76474be4c344035c043a682
chebm <chebm@MARU>
2024-11-13 22:21:29 +0100

    add application to infrastructure dependencies

--- MovieManagementSystem/MovieSystem.API/obj/Debug/net8.0/MovieSystem.API.AssemblyInfo.cs
+++ MovieManagementSystem/MovieSystem.API/obj/Debug/net8.0/MovieSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba8c9c1adad2434bd021f47cf820b31afcc6898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eef93716530020b56dfede2428cb4aebaa652ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MovieManagementSystem/MovieSystem.Infrastructure/obj/Debug/net8.0/MovieSystem.Infrastructure.AssemblyInfo.cs
+++ MovieManagementSystem/MovieSystem.Infrastructure/obj/Debug/net8.0/MovieSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27c6588e34de6a78d52744876ac9714f46ac22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eef93716530020b56dfede2428cb4aebaa652ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
